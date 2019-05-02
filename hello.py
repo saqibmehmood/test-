00fb01dc8fd1d39220adbe2c5fef6cfb548fcd2e
Saqib Mehmood <saqibmehmood199@gmail.com>
2019-05-02 17:05:54 +0500

    Merge

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@ print("I am going to GitHub")
 
 
 new = "Now add some more changes to this file"
+<<<<<<< HEAD
 print("This is master branch")
+=======
+print("Thirsty Branch")
+print("we are working on Branch")
+print("2nd branch.")
+>>>>>>> thirsty
